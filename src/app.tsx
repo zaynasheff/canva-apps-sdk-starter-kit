cb541f75edf40f4b6500ddfdff6af01047604d4c
zaynasheff <zaynasheff@gmail.com>
2024-06-06 10:40:06 +0300

    initial

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,26 @@
-import { Button, Rows, Text } from "@canva/app-ui-kit";
-import { addNativeElement } from "@canva/design";
-import * as React from "react";
+import React from "react";
+import { requestExport } from "@canva/design";
 import styles from "styles/components.css";
+import { Button, Rows, Text } from "@canva/app-ui-kit";
 
-export const App = () => {
-  const onClick = () => {
-    addNativeElement({
-      type: "TEXT",
-      children: ["Hello world!"],
+export function App() {
+
+  console.log('test react');
+
+    async function handleClick() {
+    const result = await requestExport({
+      acceptedFileTypes: ["VIDEO"],
     });
-  };
+
+    console.log(result);
+   
+  }
 
   return (
     <div className={styles.scrollContainer}>
       <Rows spacing="2u">
-        <Text>
-          To make changes to this app, edit the <code>src/app.tsx</code> file,
-          then close and reopen the app in the editor to preview the changes.
-        </Text>
-        <Button variant="primary" onClick={onClick} stretch>
-          Do something cool
-        </Button>
+      <Button variant="primary"  onClick={handleClick} stretch>Export design</Button>
       </Rows>
     </div>
   );
-};
+}
